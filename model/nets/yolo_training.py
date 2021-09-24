2a3d241914e365eea6b5091cf381525761f75510
sayosaru <qekatalyx@gmail.com>
2021-09-24 15:29:16 +0800

    Update yolo_training.py

--- model/nets/yolo_training.py
+++ model/nets/yolo_training.py
@@ -1,9 +1,9 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+import paddle
+import paddle.nn as nn
+import paddle.nn.functional as F
 
 
-class IOUloss(nn.Module):
+class IOUloss(nn.Layer):
     def __init__(self, reduction="none", loss_type="iou"):
         super(IOUloss, self).__init__()
         self.reduction = reduction
@@ -14,32 +14,32 @@ class IOUloss(nn.Module):
 
         pred = pred.view(-1, 4)
         target = target.view(-1, 4)
-        tl = torch.max(
+        tl = paddle.maximum(
             (pred[:, :2] - pred[:, 2:] / 2), (target[:, :2] - target[:, 2:] / 2)
         )
-        br = torch.min(
+        br = paddle.minimum(
             (pred[:, :2] + pred[:, 2:] / 2), (target[:, :2] + target[:, 2:] / 2)
         )
 
-        area_p = torch.prod(pred[:, 2:], 1)
-        area_g = torch.prod(target[:, 2:], 1)
+        area_p = paddle.prod(pred[:, 2:], 1)
+        area_g = paddle.prod(target[:, 2:], 1)
 
-        en = (tl < br).type(tl.type()).prod(dim=1)
-        area_i = torch.prod(br - tl, 1) * en
+        en = (tl < br).type(tl.type()).prod(axis=1)
+        area_i = paddle.prod(br - tl, 1) * en
         iou = (area_i) / (area_p + area_g - area_i + 1e-16)
 
         if self.loss_type == "iou":
             loss = 1 - iou ** 2
         elif self.loss_type == "giou":
-            c_tl = torch.min(
+            c_tl = paddle.minimum(
                 (pred[:, :2] - pred[:, 2:] / 2), (target[:, :2] - target[:, 2:] / 2)
             )
-            c_br = torch.max(
+            c_br = paddle.maximum(
                 (pred[:, :2] + pred[:, 2:] / 2), (target[:, :2] + target[:, 2:] / 2)
             )
-            area_c = torch.prod(c_br - c_tl, 1)
-            giou = iou - (area_c - area_i) / area_c.clamp(1e-16)
-            loss = 1 - giou.clamp(min=-1.0, max=1.0)
+            area_c = paddle.prod(c_br - c_tl, 1)
+            giou = iou - (area_c - area_i) / area_c.clip(1e-16)
+            loss = 1 - giou.clip(min=-1.0, max=1.0)
 
         if self.reduction == "mean":
             loss = loss.mean()
@@ -49,7 +49,7 @@ class IOUloss(nn.Module):
         return loss
 
 
-class YOLOLoss(nn.Module):
+class YOLOLoss(nn.Layer):
     def __init__(self, num_classes, strides=[8, 16, 32]):
         super().__init__()
         self.num_classes = num_classes
@@ -57,7 +57,7 @@ class YOLOLoss(nn.Module):
 
         self.bcewithlog_loss = nn.BCEWithLogitsLoss(reduction="none")
         self.iou_loss = IOUloss(reduction="none")
-        self.grids = [torch.zeros(1)] * len(strides)
+        self.grids = [paddle.zeros([1])] * len(strides)
 
     def forward(self, inputs, labels=None):
         outputs = []
@@ -80,23 +80,23 @@ class YOLOLoss(nn.Module):
             output, grid = self.get_output_and_grid(output, k, stride)
             x_shifts.append(grid[:, :, 0])
             y_shifts.append(grid[:, :, 1])
-            expanded_strides.append(torch.ones_like(grid[:, :, 0]) * stride)
+            expanded_strides.append(paddle.ones_like(grid[:, :, 0]) * stride)
             outputs.append(output)
 
-        return self.get_losses(x_shifts, y_shifts, expanded_strides, labels, torch.cat(outputs, 1))
+        return self.get_losses(x_shifts, y_shifts, expanded_strides, labels, paddle.concat(outputs, 1))
 
     def get_output_and_grid(self, output, k, stride):
         grid = self.grids[k]
         hsize, wsize = output.shape[-2:]
         if grid.shape[2:4] != output.shape[2:4]:
-            yv, xv = torch.meshgrid([torch.arange(hsize), torch.arange(wsize)])
-            grid = torch.stack((xv, yv), 2).view(1, hsize, wsize, 2).type(output.type())
+            yv, xv = paddle.meshgrid([paddle.arange(hsize), paddle.arange(wsize)])
+            grid = paddle.stack((xv, yv), 2).view(1, hsize, wsize, 2).type(output.type())
             self.grids[k] = grid
         grid = grid.view(1, -1, 2)
 
         output = output.flatten(start_dim=2).permute(0, 2, 1)
         output[..., :2] = (output[..., :2] + grid) * stride
-        output[..., 2:4] = torch.exp(output[..., 2:4]) * stride
+        output[..., 2:4] = paddle.exp(output[..., 2:4]) * stride
         return output, grid
 
     def get_losses(self, x_shifts, y_shifts, expanded_strides, labels, outputs):
@@ -119,9 +119,9 @@ class YOLOLoss(nn.Module):
         #   y_shifts            [1, n_anchors_all]
         #   expanded_strides    [1, n_anchors_all]
         # -----------------------------------------------#
-        x_shifts = torch.cat(x_shifts, 1)
-        y_shifts = torch.cat(y_shifts, 1)
-        expanded_strides = torch.cat(expanded_strides, 1)
+        x_shifts = paddle.concat(x_shifts, 1)
+        y_shifts = paddle.concat(y_shifts, 1)
+        expanded_strides = paddle.concat(expanded_strides, 1)
 
         cls_targets = []
         reg_targets = []
@@ -155,9 +155,9 @@ class YOLOLoss(nn.Module):
                     cls_preds_per_image, obj_preds_per_image,
                     expanded_strides, x_shifts, y_shifts,
                 )
-                torch.cuda.empty_cache()
+
                 num_fg += num_fg_img
-                cls_target = F.one_hot(gt_matched_classes.to(torch.int64),
+                cls_target = F.one_hot(gt_matched_classes.to_tensor(dtype=paddle.int64),
                                        self.num_classes).float() * pred_ious_this_matching.unsqueeze(-1)
                 obj_target = fg_mask.unsqueeze(-1)
                 reg_target = gt_bboxes_per_image[matched_gt_inds]
@@ -166,10 +166,10 @@ class YOLOLoss(nn.Module):
             obj_targets.append(obj_target.type(cls_target.type()))
             fg_masks.append(fg_mask)
 
-        cls_targets = torch.cat(cls_targets, 0)
-        reg_targets = torch.cat(reg_targets, 0)
-        obj_targets = torch.cat(obj_targets, 0)
-        fg_masks = torch.cat(fg_masks, 0)
+        cls_targets = paddle.concat(cls_targets, 0)
+        reg_targets = paddle.concat(reg_targets, 0)
+        obj_targets = paddle.concat(obj_targets, 0)
+        fg_masks = paddle.concat(fg_masks, 0)
 
         num_fg = max(num_fg, 1)
         loss_iou = (self.iou_loss(bbox_preds.view(-1, 4)[fg_masks], reg_targets)).sum()
@@ -180,7 +180,7 @@ class YOLOLoss(nn.Module):
 
         return loss / num_fg
 
-    @torch.no_grad()
+    @paddle.no_grad()
     def get_assignments(self, num_gt, total_num_anchors, gt_bboxes_per_image, gt_classes, bboxes_preds_per_image,
                         cls_preds_per_image, obj_preds_per_image, expanded_strides, x_shifts, y_shifts):
         # -------------------------------------------------------#
@@ -249,7 +249,7 @@ class YOLOLoss(nn.Module):
 
             area_a = paddle.prod(bboxes_a[:, 2:], 1)
             area_b = paddle.prod(bboxes_b[:, 2:], 1)
-        en = (tl < br).type(tl.type()).prod(dim=2)
+        en = (tl < br).type(tl.type()).prod(axis=2)
         area_i = paddle.prod(br - tl, 2) * en
         return area_i / (area_a[:, None] + area_b - area_i)
 
@@ -289,8 +289,8 @@ class YOLOLoss(nn.Module):
         #   is_in_boxes     [num_gt, n_anchors_all]
         #   is_in_boxes_all [n_anchors_all]
         # -------------------------------------------------------#
-        is_in_boxes = bbox_deltas.min(dim=-1).values > 0.0
-        is_in_boxes_all = is_in_boxes.sum(dim=0) > 0
+        is_in_boxes = bbox_deltas.min(axis=-1).values > 0.0
+        is_in_boxes_all = is_in_boxes.sum(axis=0) > 0
 
         gt_bboxes_per_image_l = (gt_bboxes_per_image[:, 0]).unsqueeze(1).repeat(1,
                                                                                 total_num_anchors) - center_radius * expanded_strides_per_image.unsqueeze(
@@ -397,16 +397,16 @@ def weights_init(net, init_type='normal', init_gain=0.02):
         classname = m.__class__.__name__
         if hasattr(m, 'weight') and classname.find('Conv') != -1:
             if init_type == 'normal':
-                torch.nn.init.normal_(m.weight.data, 0.0, init_gain)
+                m.weight.data = paddle.nn.initializer.Normal( 0.0, init_gain)
             elif init_type == 'xavier':
-                torch.nn.init.xavier_normal_(m.weight.data, gain=init_gain)
+                m.weight.data = paddle.nn.initializer.XavierNormal( gain=init_gain)
             elif init_type == 'kaiming':
-                torch.nn.init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+                m.weight.data = paddle.nn.initializer.KaimingNormal( a=0, mode='fan_in')
             else:
                 raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
         elif classname.find('BatchNorm2d') != -1:
-            torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
-            torch.nn.init.constant_(m.bias.data, 0.0)
+            m.weight.data = paddle.nn.initializer.Normal( 1.0, 0.02)
+            m.bias.data = paddle.nn.initializer.Constant( 0.0)
 
     print('initialize network with %s type' % init_type)
     net.apply(init_func)
